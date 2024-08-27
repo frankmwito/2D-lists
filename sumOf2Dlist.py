f8e458436e18842e1fc53ddcb281936155dbee56
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-27 09:23:37 +0300

    sum of all elements in a 2D list

--- sumOf2Dlist.py
+++ sumOf2Dlist.py
@@ -1,1 +1,21 @@
 # calculate the sum of all elements in a 2D list
+
+matrix = []
+
+for i in range(3):
+    row = []
+    for j in range(3):
+        number = int(input(f"Enter any number ({i+1}, {j+1}):"))
+        row.append(number)
+    matrix.append(row)
+    
+
+total_sum = 0
+
+for row in matrix:
+    for element in row:
+        total_sum += element
+        
+        
+        
+print(f"The sum of all elements in the matrix is: {total_sum}")
